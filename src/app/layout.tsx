a4e6b08737778930628c26b7ba54f90397e49179
Mortez-usa <hatam.info@gmail.com>
2024-01-11 14:38:47 -0800

    vercel/analytics/react added to layout page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next'
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/react';
+
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from './components/Navbar'
