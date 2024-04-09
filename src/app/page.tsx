3a533953e7c750f85bbcc2311d3af5414a023eb2
Mortez-usa <hatam.info@gmail.com>
2024-04-09 14:09:36 -0700

    google analytic env

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,13 +9,13 @@ export default function Home() {
 		<>
 			<Script
 				async
-				src={`https://www.googletagmanager.com/gtag/js?id=G-FGHHHD31CJ`}></Script>
+				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}></Script>
 			<Script id='google analytics' strategy='afterInteractive'>
 				{`window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-FGHHHD31CJ');`}
+  gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');`}
 			</Script>
 			<div className='min-h-screen'>
 				<Index />
