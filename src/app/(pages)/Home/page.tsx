78e14688bc0be3dd0deeb7a3acf44e906b3098f4
Mortez-usa <hatam.info@gmail.com>
2024-04-09 13:39:38 -0700

    google analytics g

--- src/app/(pages)/Home/page.tsx
+++ src/app/(pages)/Home/page.tsx
@@ -4,6 +4,19 @@ import Script from 'next/script';
 
 export default function page() {
   return (
+	<>
+	      <Script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      ></Script>
+      <Script id="google analytics" strategy="afterInteractive">
+        {`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');`}
+      </Script>
+
 		<div>
 			
 			<div className=''>
@@ -46,5 +59,6 @@ export default function page() {
 				</div>
 			</div>
 		</div>
+	</>
   );
 }
