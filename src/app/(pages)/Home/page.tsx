d8fc415e9a3282a918a4193ac49918a98b1f4764
Mortez-usa <hatam.info@gmail.com>
2024-04-09 15:14:30 -0700

    google analyrics new env

--- src/app/(pages)/Home/page.tsx
+++ src/app/(pages)/Home/page.tsx
@@ -1,67 +1,63 @@
-import React from 'react'
+import React from 'react';
 import Image from 'next/image';
 import Script from 'next/script';
 
 export default function page() {
-  return (
-	<>
-	<div>
-		{/*google analytics*/}
-	</div>
-	      <Script
-        async
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-      ></Script>
-      <Script id="google analytics" strategy="afterInteractive">
-        {`window.dataLayer = window.dataLayer || [];
+	return (
+		<>
+			<div>{/*google analytics*/}</div>
+			<Script
+				async
+				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}></Script>
+			<Script id='google analytics' strategy='afterInteractive'>
+				{`window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');`}
-      </Script>
+			</Script>
 
-		<div>
-			
-			<div className=''>
-				<div className='flex justify-center gap-14 mt-36 text-gray-700'>
-					<h1 className='w-[400px]'>
-						Hello, I&#39;m Morteza Maddahi, an individual with a
-						background in{' '}
-						<a
-							className='text-blue-500'
-							href='https://archinect.com/Morteza-Maddahi'>
-							architecture design
-						</a>{' '}
-						who also has a keen interest in entrepreneurship,{' '}
-						<a
-							className='text-blue-500'
-							href='https://www.docktorly.com'>
-							software
-						</a>{' '}
-						, and emerging technologies. I have a genuine passion
-						for nature, enjoy watching{' '}
-						<a
-							className='text-blue-500'
-							href='https://editorial.rottentomatoes.com/guide/essential-sci-fi-movies-of-all-time/'>
-							Science fiction
-						</a>{' '}
-						movies, and place great value on cultivating strong
-						relationships with friends . Join me as I explore the
-						harmonious blend of creativity, technology, and a love
-						for life&#39;s simple pleasures.
-					</h1>
-					<div className='grid-col-2'>
-						<Image
-							src='/images/Morteza-pro.jpg'
-							width={200}
-							height={200}
-							alt='Picture of the author'
-							className='rounded-lg shadow-sm hover:opacity-50'
-						/>
+			<div>
+				<div className=''>
+					<div className='flex justify-center gap-14 mt-36 text-gray-700'>
+						<h1 className='w-[400px]'>
+							Hello, I&#39;m Morteza Maddahi, an individual with a
+							background in{' '}
+							<a
+								className='text-blue-500'
+								href='https://archinect.com/Morteza-Maddahi'>
+								architecture design
+							</a>{' '}
+							who also has a keen interest in entrepreneurship,{' '}
+							<a
+								className='text-blue-500'
+								href='https://www.docktorly.com'>
+								software
+							</a>{' '}
+							, and emerging technologies. I have a genuine
+							passion for nature, enjoy watching{' '}
+							<a
+								className='text-blue-500'
+								href='https://editorial.rottentomatoes.com/guide/essential-sci-fi-movies-of-all-time/'>
+								Science fiction
+							</a>{' '}
+							movies, and place great value on cultivating strong
+							relationships with friends . Join me as I explore
+							the harmonious blend of creativity, technology, and
+							a love for life&#39;s simple pleasures.
+						</h1>
+						<div className='grid-col-2'>
+							<Image
+								src='/images/Morteza-pro.jpg'
+								width={200}
+								height={200}
+								alt='Picture of the author'
+								className='rounded-lg shadow-sm hover:opacity-50'
+							/>
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-	</>
-  );
+		</>
+	);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,13 +9,13 @@ export default function Home() {
 		<>
 			<Script
 				async
-				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}></Script>
+				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}></Script>
 			<Script id='google analytics' strategy='afterInteractive'>
 				{`window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');`}
+  gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');`}
 			</Script>
 			<div className='min-h-screen'>
 				<Index />
