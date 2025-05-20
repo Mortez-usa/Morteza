8939fe67b2fbd367e90c321ce99daa7b1053ac7c
Mortez-usa <hatam.info@gmail.com>
2025-05-19 20:36:53 -0700

    Adds utility function to capitalize the first letter of a string
    Introduces a `capitalizeFirst` function to transform a string by making its first letter uppercase and the rest lowercase. This utility enhances string formatting consistency and reusability.

--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -36,7 +36,7 @@ export default function ContactForm() {
 		}
 		setLoading(false);
 	}
-
+	//capitalizeFirst function to capitalize the first letter of a string
 	const capitalizeFirst = (str: string) => {
 		if (!str) return '';
 		return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
