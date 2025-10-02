e946d04ae9f183dd51e41b281847cdb79daf066e
Mortez-usa <hatam.info@gmail.com>
2025-10-02 00:35:44 -0700

    fixing props

--- src/app/(pages)/Blog/post/[slug]/page.tsx
+++ src/app/(pages)/Blog/post/[slug]/page.tsx
@@ -1,11 +1,7 @@
 'use server';
 import { supabase } from '@/lib/supabase/client';
 
-type Props = {
-	params: { slug: string };
-};
-
-export default async function Posts({ params }: Props) {
+export default async function Posts({ params }: { params: { slug: string } }) {
 	const slug = params?.slug ?? '';
 
 	// Try to fetch by explicit `slug` column first
